c3ffd3d8580312079fc1b6fdb60895b5edf070f5
Miroslav <savovsky@gmail.com>
2019-02-08 18:07:43 +0200

    prepare for heroku - fix 2.

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/dist'));
 
 app.all('*', (req, res) => {
-    res.status(200).sendFile(__dirname + '/dist/index.html'));
+    res.status(200).sendFile(__dirname + '/dist/index.html');
 });
 
 app.listen(port, () => {
