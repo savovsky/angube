c83a064390be9683d6d3ed2a4401e9dde7d1cf90
Miroslav Savovski <miroslav.savovski@scalefocus.com>
2019-03-06 18:05:34 +0200

    Playing with tabs.

--- src/app/mat-components.module.ts
+++ src/app/mat-components.module.ts
@@ -9,7 +9,8 @@ import {
   MatProgressBarModule,
   MatMenuModule,
   MatInputModule,
-  MatCardModule
+  MatCardModule,
+  // MatTabsModule
 } from '@angular/material';
 
 
@@ -23,7 +24,8 @@ import {
     MatListModule,
     MatMenuModule,
     MatProgressBarModule,
-    MatCardModule
+    MatCardModule,
+    // MatTabsModule
   ]
 })
 export class MatComponentsModule { }
