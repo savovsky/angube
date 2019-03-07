35ec5002ee2031e07c4a84e877d81d35bfccf846
Miroslav Savovski <miroslav.savovski@scalefocus.com>
2019-03-07 12:42:57 +0200

    Service - Template data binding

--- src/app/service/http-response.service.ts
+++ src/app/service/http-response.service.ts
@@ -3,6 +3,8 @@ import { Subject, AsyncSubject } from 'rxjs';
 // @Injectable({
 //   providedIn: 'root' // TODO What is this?
 // })
+// https://www.udemy.com/the-complete-guide-to-angular-2/learn/v4/t/lecture/5401644?start=0
+
 export class HttpResponseService {
     signUpUserSuccess = new Subject();
     signUpUserError = new Subject();  // TODO Is there a better way?
