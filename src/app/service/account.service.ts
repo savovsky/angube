5db5678d7972ba7378a1e531e50eead886abff7a
Miroslav Savovski <miroslav.savovski@scalefocus.com>
2019-03-27 12:30:45 +0200

    Comments - added.

--- src/app/service/account.service.ts
+++ src/app/service/account.service.ts
@@ -11,15 +11,23 @@ export class AccountService {
     'email',
     'isBlocked',
     'isAdmin',
-    'uid',
+    'uid'
   ];
 
+  /**
+   * Mutate the user account object to array of its entries and
+   * reorder this array elements by given list.
+   * @param account User account object.
+   */
   toOrderedArray(account: User) {
     this.user = Object.entries(account);
     this.orderByList();
     return this.user;
   }
 
+  /**
+   * Reorder user array elements by index from 'orderList' array.
+   */
   orderByList() {
     this.orderList.forEach((el, index) => {
       const currentItemIndex = this.user.findIndex((item) => item[0] === el);
