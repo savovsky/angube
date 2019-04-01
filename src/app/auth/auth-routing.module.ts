d894f422d0c7ff27e88e2400cb8db29fc40a6e53
Miroslav Savovski <miroslav.savovski@scalefocus.com>
2019-04-01 09:49:00 +0300

    AuthRoutingModule - added and wired up.

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,73 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './components/welcome/welcome.component';
-import { HomeComponent } from '../home/home.component';
-import { AccountComponent } from '../user/components/account/account.component';
-import { ProfileComponent } from '../user/components/profile/profile.component';
-import { UsersComponent } from '../admin/components/users/users.component';
 import { SigninComponent } from './components/signin/signin.component';
 import { SignupComponent } from './components/signup/signup.component';
-import { NotFoundComponent } from '../not-found/not-found.component';
 import { QuestionComponent } from './components/question/question.component';
-import { ApplicationComponent } from '../application/application.component';
-import { AccessDeniedComponent } from '../admin/components/access-denied/access-denied.component';
-import { AuthGuard } from './guards/auth.guard';
-import { AdminGuard } from '../admin/guards/admin.guard';
-import { DashboardComponent } from '../user/components/dashboard/dashboard.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: WelcomeComponent
-  },
-  {
-    path: 'app',
-    canActivateChild: [AuthGuard],
-    component: ApplicationComponent,
-    children: [
-      {
-        path: 'home',
-        component: HomeComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: 'account/:id/:username',
-        component: AccountComponent
-      },
-      {
-        path: 'account/:username',
-        component: AccountComponent
-      },
-      {
-        path: 'account',
-        component: AccountComponent
-      },
-      {
-        path: 'users/:id/:username',
-        component: ProfileComponent
-      },
-      {
-        path: 'users/:username',
-        component: ProfileComponent
-      },
-      {
-        path: 'users',
-        canActivate: [AdminGuard],
-        component: UsersComponent
-      },
-      {
-        path: 'access-denied',
-        component: AccessDeniedComponent
-      }
-    ]
-  },
-  {
-    path: 'question',
-    component: QuestionComponent
   },
   {
     path: 'signin',
@@ -78,17 +20,13 @@ const routes: Routes = [
     component: SignupComponent
   },
   {
-    path: 'not-found',
-    component: NotFoundComponent
-  },
-  {
-    path: '**',
-    redirectTo: '/not-found'
+    path: 'question',
+    component: QuestionComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AuthRoutingModule { }
--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { WelcomeComponent } from '../auth/components/welcome/welcome.component';
 import { HomeComponent } from '../home/home.component';
 import { AccountComponent } from '../user/components/account/account.component';
 import { ProfileComponent } from '../user/components/profile/profile.component';
 import { UsersComponent } from '../admin/components/users/users.component';
-import { SigninComponent } from '../auth/components/signin/signin.component';
-import { SignupComponent } from '../auth/components/signup/signup.component';
 import { NotFoundComponent } from '../not-found/not-found.component';
-import { QuestionComponent } from '../auth/components/question/question.component';
 import { ApplicationComponent } from '../application/application.component';
 import { AccessDeniedComponent } from '../admin/components/access-denied/access-denied.component';
 import { AuthGuard } from '../auth/guards/auth.guard';
@@ -17,10 +13,6 @@ import { DashboardComponent } from '../user/components/dashboard/dashboard.compo
 
 
 const routes: Routes = [
-  {
-    path: '',
-    component: WelcomeComponent
-  },
   {
     path: 'app',
     canActivateChild: [AuthGuard],
@@ -64,18 +56,6 @@ const routes: Routes = [
         component: AccessDeniedComponent
       }
     ]
-  },
-  {
-    path: 'question',
-    component: QuestionComponent
-  },
-  {
-    path: 'signin',
-    component: SigninComponent
-  },
-  {
-    path: 'signup',
-    component: SignupComponent
   },
   {
     path: 'not-found',
