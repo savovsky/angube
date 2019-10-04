16c19de6101f96b56c453bdf3ea0e28ba2e53768
Miroslav Savovski <miroslav.savovski@scalefocus.com>
2019-10-04 18:31:42 +0300

    ProgressSpinnerComponent - style.

--- src/app/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/dashboard/components/dashboard/dashboard.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { DatabaseDashboardService } from './../../../shared/services/database-dashboard.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent { }
+export class DashboardComponent implements OnInit, OnDestroy {
+
+  isFetching: boolean;
+  databaseDashboardUpdateSubscription: Subscription;
+
+  constructor(
+    public databaseDashboardService: DatabaseDashboardService
+  ) { }
+
+  ngOnInit() {
+    this.isFetching = true;
+    this.databaseDashboardUpdateSubscription = this.databaseDashboardService.databaseDashboardUpdate.subscribe(
+      () => { this.isFetching = false; }
+    );
+  }
+
+  ngOnDestroy() {
+    this.databaseDashboardUpdateSubscription.unsubscribe();
+  }
+
+}
